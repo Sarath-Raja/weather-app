5532972bb86f3e86817a44e19494525caa414ff0
Sarath Raja <sarathrajahbk@gmail.com>
2021-02-08 01:54:21 +0530

    Added all remaining files

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+console.log('Javascript file for client side rendering')
+
+const weaterForm = document.querySelector('form');
+const search = document.querySelector('input');
+const message1 = document.querySelector('#message-1');
+const message2 = document.querySelector('#message-2');
+
+weaterForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    message1.textContent = "Loading.....";
+    message2.textContent = "";
+    fetch(`http://localhost:3000/weather?address=${search.value}`).then((response) => {
+        response.json().then((weatherData) => {
+            if(weatherData.error)
+                message1.textContent = weatherData.error;
+            else
+            {
+                message1.textContent = weatherData.location;
+                message2.textContent = weatherData.forecast;
+            }
+        })
+    });
+})
